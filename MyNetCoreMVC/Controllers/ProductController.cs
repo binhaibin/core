2d1eddf3ca7fc2000cebe23acb634702f80f284b
unknown <binhaibin>
2018-10-27 21:27:44 +0700

    2

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetList()
+        {
+            return View();
+        }
+
+        public IActionResult Create(string name , int price)
+        {
+            ViewData["Name"] = name;
+            ViewData["Price"] = price;
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+           return new JsonResult(new Product
+               {
+               Id = id
+               });
+        }
+    }
+}
